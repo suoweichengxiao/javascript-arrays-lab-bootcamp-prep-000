93b0c3bcac5a857f5a02ceb1714112fd94340a41
Joe Dettmar <joedettmar@gmail.com>
2017-11-03 15:33:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {kittens.push(name);}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyPrependKitten(name) {kittens.unshift(name);}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveLastKitten() {kittens.pop();}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveFirstKitten() {kittens.shift();}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name) {var newKittens = kittens.append(name);
+  return newKittens;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function prependKitten(name) {var newKittens = kittens.prepend(name);}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function removeLastKitten() {var newKittens = kittens.slice(0, -1);}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function removeFirstKitten() {var newKittens = kittens.slice(0, kittens.length);
+}
